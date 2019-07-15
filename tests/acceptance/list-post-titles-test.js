78cc8f16ae0be82251f94b532940ca6c5460edb4
Javi Carrion <suavejavi@google.com>
2019-07-15 19:28:38 -0400

    test: add new path for the acceptance tests
    Add TODO reminders for text and post template

--- tests/acceptance/list-post-titles-test.js
+++ tests/acceptance/list-post-titles-test.js
@@ -12,7 +12,7 @@ module('Acceptance | list post titles', function(hooks) {
   test('should show list of post titles on the home page', async function(assert) {
     await visit('/');
 
-    assert.equal(currentURL(), '/list', 'should redirect automatically to the list of post title');
+    assert.equal(currentURL(), '/post', 'should redirect automatically to the list of post title');
   });
 
   test('should link to information about me', async function(assert) {
@@ -21,13 +21,18 @@ module('Acceptance | list post titles', function(hooks) {
     assert.equal(currentURL(), '/about', 'should navigate to about me');
   });
 
-  test('should list post TEST titles', async function(assert) {
+  test('should list post titles', async function(assert) {
     await visit('/');
-    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+    assert.equal(this.element.querySelectorAll('.listing').length, 100, 'should display 3 listings');
   });
 
   test('should show more details for list of post titles', async function(assert) {
-    
+/*     TODO: Example test
+    await visit('/rentals');
+    await click(".grand-old-mansion");
+    assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
+    assert.ok(this.element.querySelector('.show-listing h2').textContent.includes('Grand Old Mansion'), 'should list rental title');
+    assert.ok(this.element.querySelector('.show-listing .description'), 'should list a description of the property'); */
   });
 
 });
