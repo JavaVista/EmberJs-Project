b09a9e40158809e1a85192bbc9ca82fe16aab947
Javi Carrion <suavejavi@google.com>
2019-07-15 13:01:10 -0400

    refactor: add user model relationship to post
    User has a many relationship to post.

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
+    post: DS.hasMany('post'),
+
     name: DS.attr('string'),
     email: DS.attr('string'),
     phone: DS.attr('string'),
