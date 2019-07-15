b86575d1321af6310df01f80f4947e064c22b00c
Javi Carrion <suavejavi@google.com>
2019-07-15 18:48:26 -0400

    feat: add serializer to display JSON

--- app/models/user.js
+++ app/models/user.js
@@ -4,8 +4,11 @@ const { Model } = DS;
 export default Model.extend({
     post: DS.hasMany('post'),
 
+    username: DS.attr('string'),
     name: DS.attr('string'),
     email: DS.attr('string'),
     phone: DS.attr('string'),
-    website: DS.attr('string')
+    website: DS.attr('string'),
+    address: DS.attr(),
+    company: DS.attr()
 });
