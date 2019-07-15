20cdfb80003f2e78754aa7ff3674c1c32827255c
Javi <suavejavi@gmail.com>
2019-07-14 20:28:56 -0400

    feat: add list route
    Modify acceptance test to take into consideration the new redirect to
     the list component.

--- tests/acceptance/list-post-titles-test.js
+++ tests/acceptance/list-post-titles-test.js
@@ -1,18 +1,18 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import {
+  click,
+  currentURL,
+  visit
+} from '@ember/test-helpers';
 
 module('Acceptance | list post titles', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should show home page = /', async function(assert) {
+  test('should show list of post titles on the home page', async function(assert) {
     await visit('/');
 
-    assert.equal(currentURL(), '/');
-  });
-
-  test('should show list of post titles on the home page', async function(assert) {
-    
+    assert.equal(currentURL(), '/list', 'should redirect automatically to the list of post title');
   });
 
   test('should link to information about me', async function(assert) {
