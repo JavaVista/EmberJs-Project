9021652102eef493bf31ff06d89ca2ac79c87302
Javi Carrion <suavejavi@google.com>
2019-07-15 12:54:12 -0400

    refactor: add post model relationship to user
    Post has one-one relationship with user.

--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
+    userId: DS.belongsTo('user'),
+    
     title: DS.attr('string'),
     body:  DS.attr('string')
 });
